d12f2b13e25379bcf15f382de4eeddf244db9fbd
Paulo Jorge Reis Pereira <paulo_pereira_6@hotmail.com>
2015-11-17 13:11:43 +0000

    updated project

--- final_projeto/P2P/app/src/main/java/com/example/asus/p2p/MainActivity.java
+++ final_projeto/P2P/app/src/main/java/com/example/asus/p2p/MainActivity.java
@@ -24,31 +24,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO
-
-        if (false) {
+        PackageManager pm = this.getPackageManager();
+        // Check whether NFC is available on device
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            // NFC is not available on the device.
             Toast.makeText(this, "The device does not has NFC hardware.",
                     Toast.LENGTH_SHORT).show();
 
-            // close app
             Intent homeIntent = new Intent(Intent.ACTION_MAIN);
             homeIntent.addCategory( Intent.CATEGORY_HOME );
             homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(homeIntent);
         }
-        // Android 4.1(JELLY_BEAN) or higher
-        else if (false) {
+        // Check whether device is running Android 4.1 or higher
+        else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            // Android Beam feature is not supported.
             Toast.makeText(this, "Android Beam is not supported.",
                     Toast.LENGTH_SHORT).show();
 
-            // close app
             Intent homeIntent = new Intent(Intent.ACTION_MAIN);
             homeIntent.addCategory( Intent.CATEGORY_HOME );
             homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(homeIntent);
         }
         else {
-            // NFC and Android Beam are supported.
+            // NFC and Android Beam file transfer is supported.
             Toast.makeText(this, "Android Beam is supported on your device.",
                     Toast.LENGTH_SHORT).show();
 
@@ -66,22 +66,35 @@ public class MainActivity extends AppCompatActivity {
     private void sendNumber() {
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
+        // Check whether NFC is enabled on device
         if(!nfcAdapter.isEnabled()){
+            // NFC is disabled, show the settings UI
+            // to enable NFC
             Toast.makeText(this, "Please enable NFC.",
                     Toast.LENGTH_SHORT).show();
-
-            // TODO
+            startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
         }
+        // Check whether Android Beam feature is enabled on device
         else if(!nfcAdapter.isNdefPushEnabled()) {
+            // Android Beam is disabled, show the settings UI
+            // to enable Android Beam
             Toast.makeText(this, "Please enable Android Beam.",
                     Toast.LENGTH_SHORT).show();
-
-            // TODO
+            startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
         }
         else {
 
-            // TODO
+            nfcAdapter.setNdefPushMessageCallback(new NfcAdapter.CreateNdefMessageCallback()
+            {
+                @Override
+                public NdefMessage createNdefMessage(NfcEvent event) {
+                    EditText editText = (EditText) findViewById(R.id.editText);
+                    //NdefRecord uriRecord = NdefRecord.createUri(Uri.encode("http://www.google.com/"));
+                    NdefRecord uriRecord = NdefRecord.createUri("tel:"+editText.getText());
+                    return new NdefMessage(new NdefRecord[] { uriRecord });
+                }
 
+            }, this, this);
             Toast.makeText(this, "Message written!", Toast.LENGTH_SHORT).show();
         }
     }
